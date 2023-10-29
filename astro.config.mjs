016b400e76e0681460e469cb2f0dbb05d0f2497d
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2023-10-28 15:10:27 -1000

    Fix Image component rendering using Bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  image: {
+    service: passthroughImageService(),
+  },
 });
