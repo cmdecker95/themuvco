bc62cc8e0f7b881a37f3b427af2e20cb05409810
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2024-01-04 22:15:07 -1000

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import { defineConfig, passthroughImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  integrations: [tailwind(), svelte()],
-  output: "server",
-  adapter: vercel(),
-  image: {
-    service: passthroughImageService(),
-  },
-});
+// https://astro.build/config
+export default defineConfig({});
